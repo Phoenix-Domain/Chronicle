2b43e793a7e2174a051d56237cd22833b9a2c4aa
Batubo Victory <victorybatubo76@gmail.com>
2025-08-04 17:05:01 +0100

    style App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import DashBoard from './components/Dashboard';
 
 function App() {
   return (
-    <>
+    <div className='bg-[#111827] text-[#F9FAFB]'>
       <Header />
       <DashBoard />
-    </>
+    </div>
   )
 }
 
