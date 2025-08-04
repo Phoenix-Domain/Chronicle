f5b8b2ba5ac4a808a7191568eed7347ad760163c
Batubo Victory <victorybatubo76@gmail.com>
2025-08-04 16:09:28 +0100

    Add content to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,15 @@
 function Header(){
-  
+  return(
+    <>
+      <h1>Chronicle</h1>
+      <p>
+        Chronicle is a lightweight, user-friendly booking dashboard built for service-based businesses that need a simple way to manage client appointments.
+      </p>
+      <p>
+        Whether you're a hairstylist, tutor, fitness coach, or freelancer, Chronicle helps you track bookings, manage client information, and stay organized—all in one place, with no backend required.
+      </p>
+    </>
+  )
 }
+
+export default Header;
