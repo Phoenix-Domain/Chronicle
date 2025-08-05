22daf632b1dda45f5b8e737750869e9a792d380e
Batubo Victory <victorybatubo76@gmail.com>
2025-08-05 12:32:39 +0100

    Import Form component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,11 @@
 import DashHeader from "./DashHeader";
-
+import Form from "./Form";
 
 function DashBoard(){
   return(
     <div className="my-10 border-1 border-gray-500">
       <DashHeader />
+      <Form />
     </div>
   )
 }
