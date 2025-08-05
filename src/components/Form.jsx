91e899945618095d98314f2d9ffe22bb6e6000a3
Batubo Victory <victorybatubo76@gmail.com>
2025-08-05 13:34:13 +0100

    Import DOM elements

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,34 +1,45 @@
+import DOMelements from "./DOMelements";
+import { useState, useEffect } from "react";
+
 function Form (){
+const [clientInfo, setClientInfo] = useState([])
+
+useEffect(() => {
+  const { clientNameInput,clientEmailInput,clientPhoneInput,clientServiceInput,clientBookingDateInput,clientStatusInput,addClientBtn } = DOMelements()
+},[])
+
+
+
   return (
     <form action="" className="flex flex-col justify-between gap-5 p-3">
       <label className="flex flex-col">
         Full Name: 
-        <input type="text" className="border-1" />
+        <input id="name" type="text" className="border-1" />
       </label>
 
       <label className="flex flex-col">
         Email: 
-        <input type="text" className="border-1" />
+        <input id="email" type="text" className="border-1" />
       </label>
 
       <label className="flex flex-col">
         Phone Number: 
-        <input type="text" className="border-1" />
+        <input id="phone" type="text" className="border-1" />
       </label>
 
       <label className="flex flex-col">
         Service Type: 
-        <input type="text" className="border-1" />
+        <input id="service" type="text" className="border-1" />
       </label>
 
       <label className="flex flex-col">
         Booking Date: 
-        <input type="date" className="border-1" />
+        <input id="bookingDate" type="date" className="border-1" />
       </label>
 
       <label className="flex flex-col">
         Status: 
-        <select type="date" className="border-1 bg-[#0a0a23] text-white">
+        <select type="date" id="status" className="border-1 bg-[#0a0a23] text-white">
           <option value="Select Status">Select Status</option>
           <option value="Completed">Completed</option>
           <option value="Pending">Pending</option>
@@ -36,7 +47,7 @@ function Form (){
         </select>
       </label>
 
-      <button type="submit" className="border-1 py-1 px-4 w-fit rounded-full">Add Client</button>
+      <button type="submit" id="addClientBtn" className="border-1 py-1 px-4 w-fit rounded-full">Add Client</button>
     </form>
   )
 }
