a2507010cffe739310ab50540d9bade788b91f94
Batubo Victory <victorybatubo76@gmail.com>
2025-08-05 12:45:54 +0100

    Ädd Form content

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,42 @@
 function Form (){
+  return (
+    <form action="" className="flex flex-col justify-between gap-5 p-3">
+      <label className="flex flex-col">
+        Full Name: 
+        <input type="text" className="border-1" />
+      </label>
 
+      <label className="flex flex-col">
+        Email: 
+        <input type="text" className="border-1" />
+      </label>
+
+      <label className="flex flex-col">
+        Phone Number: 
+        <input type="text" className="border-1" />
+      </label>
+
+      <label className="flex flex-col">
+        Service Type: 
+        <input type="text" className="border-1" />
+      </label>
+
+      <label className="flex flex-col">
+        Booking Date: 
+        <input type="date" className="border-1" />
+      </label>
+
+      <label className="flex flex-col">
+        Status: 
+        <select type="date" className="border-1 bg-[#0a0a23] text-white">
+          <option value="Select Status">Select Status</option>
+          <option value="Completed">Completed</option>
+          <option value="Pending">Pending</option>
+          <option value="Cancelled">Cancelled</option>
+        </select>
+      </label>
+    </form>
+  )
 }
 
 export default Form;
